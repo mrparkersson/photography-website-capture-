b97e80e32dafd35d3bac26a25c82493086253d21
mrparkersson <amoparkerprince78@gmail.com>
2021-09-14 00:42:13 +0000

    photography website

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AboutUs from "./pages/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,7 @@
+import AboutSection from "../Components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
